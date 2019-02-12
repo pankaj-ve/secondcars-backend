e64a9fe79e94d1fe56c959457e01a45e09a47d2b
pankaj kumar <pankajkumar1@virtualemployee.com>
2019-02-12 12:43:33 +0530

    test

--- Utilities/config.js
+++ Utilities/config.js
@@ -14,7 +14,7 @@ let serverURLs = {
     "staging": {
         "NODE_SERVER": "http://11.11.11.11",
         "NODE_SERVER_PORT": "3000",
-        "MONGO_DB": "mongodb://deployment:kaur@123@ds331145.mlab.com:25375/secondcars",
+        "MONGO_DB": "mongodb://deployment:kaur%40123@ds331145.mlab.com:25375/secondcars",
         "EMAIL_USER": 'toothfairysanjeet@gmail.com',
         "EMAIL_PASS": 'Sanj1234A',
         "EMAIL_HOST": 'smtp.gmail.com',
